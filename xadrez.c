c272777d33d3a74938ea4040c28ee7bb07f665de
FredyH2025 <fredyhbf@yahoo.com.br>
2025-06-16 16:09:15 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -2,33 +2,33 @@
 
 int main() {
     // Movimentação da Torre: 5 casas para a direita (usando 'for')
-    int movimentoTorre = 5;
-    printf("Movimento da Torre:\n");
-    for (int i = 0; i < movimentoTorre; i++) {
-        printf("Direita\n");
+    int movimentoTorre = 5;                         // O movimentoTorre é inicializado com 5
+    printf("Movimento da Torre:\n");                
+    for (int i = 0; i < movimentoTorre; i++) {      // enquanto o i for menor movimentoTorre, a Torre se movimenta para a direita  
+        printf("Direita\n");                        // mostra todos os movimentos da Torre 
     }
 
     printf("\n");
 
     // Movimentação do Bispo: 5 casas na diagonal para cima e à direita (usando 'while')
-    int movimentoBispo = 5;
-    int contadorBispo = 0;
+    int movimentoBispo = 5;                        // O movimentoBispo é inicializado com 5
+    int contadorBispo = 0;  
     printf("Movimento do Bispo:\n");
-    while (contadorBispo < movimentoBispo) {
-        printf("Cima Direita\n");
-        contadorBispo++;
+    while (contadorBispo < movimentoBispo) {       // enquanto o contadorBispo for menor do que o movimentoBispo, o Bispo movimenta para cima direita    
+        printf("Cima Direita\n");                  // mostra dos os movimentos do Bispo  
+        contadorBispo++;                           // contadorBispo vai sendo incrementado 
     }
 
     printf("\n");
 
     // Movimentação da Rainha: 8 casas para a esquerda (usando 'do-while')
-    int movimentoRainha = 8;
+    int movimentoRainha = 8;                       // O movimentoRainha é inicializado com 8
     int contadorRainha = 0;
     printf("Movimento da Rainha:\n");
     do {
-        printf("Esquerda\n");
-        contadorRainha++;
-    } while (contadorRainha < movimentoRainha);
+        printf("Esquerda\n");                      // mostra todos os movimentos da Rainha
+        contadorRainha++;                          // contadorRainha vai sendo incrementado
+    } while (contadorRainha < movimentoRainha);    // enquanto o contadorRainha for menor do que o movimentoRainha, a Rainha se movimenta para a esquerda
 
     return 0;
 }
